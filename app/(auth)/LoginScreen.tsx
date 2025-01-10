fc5659042038526e49ac2be05bf33933bda42f0e
Developer <info@ifoxsolutions.com>
2025-01-10 10:20:17 +0530

    Work on Loginscreen

--- app/(auth)/LoginScreen.tsx
+++ app/(auth)/LoginScreen.tsx
@@ -1,22 +1,18 @@
 import React, { useRef, useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Dimensions, Text, TouchableOpacity, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { LinearGradient } from "expo-linear-gradient";
-import TextField from "../components/TextField";
 import { useForm, Controller } from "react-hook-form";
 import { REGEX } from "@/src/utills/globals";
 import CustomButton from "../components/CustomButton";
+import TextField from "../components/TextField";
 import Animated, {
     BounceInDown,
+    BounceInLeft,
+    BounceInRight,
     SlideInLeft,
     SlideInRight,
-    FadeIn,
-    Easing,
 } from "react-native-reanimated";
-
-
-
-
+const Touchable = Animated.createAnimatedComponent(TouchableOpacity);
 
 const LoginScreen: React.FC = () => {
     const {
@@ -26,175 +22,170 @@ const LoginScreen: React.FC = () => {
         formState: { errors, isValid },
     } = useForm({ mode: "all", shouldUnregister: false });
 
-    const Touchable = Animated.createAnimatedComponent(TouchableOpacity);
-
-
-
     const [showPassword, setShowPassword] = useState(false);
-    const passwordRef = useRef<any>(null);  // Ref for password input field
+    const passwordRef = useRef<any>(null);
+    const screenWidthMinus20 = Dimensions.get('window').width;
 
     const handleLogin = (data: any) => {
         console.log("Form Data:", data);
     };
 
-
     const handleGoogleLogin = async () => {
         try {
-            await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
-            // google services are available
+            console.log("Google Sign-In Triggered");
         } catch (err) {
-            console.error('play services are not available');
+            console.error("Google Sign-In Error", err);
         }
     };
 
-
     return (
-        <LinearGradient
-            colors={["#263238", "#1C1C1C", "#fff"]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            style={{ flex: 1 }}
-        >
+        <View className="flex-1">
+            {/* Background Layers */}
+            <View className="absolute inset-0 ">
+                <Animated.View
+                    className="flex-1 bg-white"
+                    entering={BounceInLeft.duration(1000).duration(200)}
+                />
+                <Animated.View
+                    className="flex-1 bg-blue-800"
+                    entering={BounceInRight.duration(1200).delay(400)}
+                />
+            </View>
+
+            {/* Login Container */}
             <KeyboardAwareScrollView
+                keyboardShouldPersistTaps="handled"
                 contentContainerStyle={{
                     flex: 1,
-                    alignItems: "center",
                     justifyContent: "center",
+                    alignItems: "center",
                 }}
-                // scrollEnabled={scrollEnabled}
-                enableResetScrollToCoords={true}
-                // extraHeight={50}
-                enableOnAndroid={false}
-                keyboardShouldPersistTaps={'handled'}
             >
                 <Animated.View
-                    className="w-full"
-                    entering={BounceInDown.duration(800)}
+                    className={`bg-white rounded-lg p-4 shadow-lg w-[90%]`}
+                    entering={BounceInDown.duration(1400).delay(600)}
                 >
-                    <View className="w-full">
-                        <View className="bg-white rounded-[12px] mx-5 p-4">
-                            <Animated.Text
-                                className="text-[32px] my-[16px] font-[Lato-Bold] text-center"
-                                entering={BounceInDown.duration(1000).delay(200)}
-                            >
-                                Login
-                            </Animated.Text>
-
-                            <Animated.View entering={SlideInLeft.duration(1200).delay(400)}>
-                                <Controller
-                                    control={control}
-                                    rules={{
-                                        required: {
-                                            value: true,
-                                            message: JSON.stringify([{ valid: false, title: "Email is Required" }]),
-                                        },
-                                        pattern: {
-                                            value: REGEX.EMAIL,
-                                            message: JSON.stringify([{ valid: false, title: "Please Enter a valid email" }]),
-                                        },
+                    <Animated.Text
+                        className={'text-[32px] my-4 text-center font-[Lato-Bold]'}
+                        entering={BounceInDown.duration(1600).delay(800)}
+                    >
+                        Login
+                    </Animated.Text>
+                    <Animated.View
+                        entering={BounceInLeft.duration(1800).delay(1000)}
+                    >
+                        <Controller
+                            control={control}
+                            rules={{
+                                required: {
+                                    value: true,
+                                    message: JSON.stringify([{ valid: false, title: "Email is Required" }]),
+                                },
+                                pattern: {
+                                    value: REGEX.EMAIL,
+                                    message: JSON.stringify([{ valid: false, title: "Please Enter a valid email" }]),
+                                },
+                            }}
+                            render={({ field: { onChange, value } }) => (
+                                <TextField
+                                    label="Email"
+                                    value={value}
+                                    rightIcon="close"
+                                    onChangeText={onChange}
+                                    onPressRightIcon={() => setValue("email", "")}
+                                    placeholder="Enter your email"
+                                    placeholderTextColor="#9E9E9E"
+                                    editable={true}
+                                    returnKeyType="next"
+                                    errorMessage={errors.email?.message}
+                                    onSubmitEditing={() => passwordRef.current?.focus()}
+                                    mainClassName={""}
+                                    onPressLeftIcon={function (): void {
+                                        throw new Error("Function not implemented.");
                                     }}
-                                    render={({ field: { onChange, value } }) => (
-                                        <TextField
-                                            label="Email"
-                                            value={value}
-                                            rightIcon="close"
-                                            onChangeText={onChange}
-                                            onPressLeftIcon={() => console.log("Left Icon Pressed")}
-                                            onPressRightIcon={() => setValue("email", "")}
-                                            placeholder="Enter your email"
-                                            placeholderTextColor="#9E9E9E"
-                                            mainClassName={""}
-                                            editable={true}
-                                            returnKeyType={'next'}
-                                            textInputStyle={undefined}
-                                            mainContainerStyle={""}
-                                            errorMessage={errors.email?.message}
-                                            onSubmitEditing={() => {
-                                                passwordRef.current?.focus();  // Focus the password field on 'next' key press
-                                            }}
-                                        />
-                                    )}
-                                    name="email"
-                                />
-                            </Animated.View>
+                                    textInputStyle={undefined} mainContainerStyle={""} />
+                            )}
+                            name="email"
+                        />
+                    </Animated.View>
+                    {/* Password Field */}
+                    <Animated.View
+                        className={'mb-4'}
+                        entering={BounceInRight.duration(2000).delay(1200)}
+                    >
+                        <Controller
+                            control={control}
+                            rules={{
+                                required: {
+                                    value: true,
+                                    message: JSON.stringify([{ valid: false, title: "Password is Required" }]),
+                                },
+                                minLength: {
+                                    value: 6,
+                                    message: JSON.stringify([{ valid: false, title: "Password must be at least 6 characters" }]),
+                                },
+                            }}
+                            render={({ field: { onChange, value } }) => (
+                                <TextField
+                                    label="Password"
+                                    value={value}
+                                    ref={passwordRef}
+                                    rightIcon={showPassword ? "eyeoff" : "eyeOn"}
+                                    onChangeText={onChange}
+                                    secureTextEntry={!showPassword}
+                                    onPressRightIcon={() => setShowPassword(!showPassword)}
+                                    placeholder="Enter your password"
+                                    placeholderTextColor="#9E9E9E"
+                                    editable={true}
+                                    returnKeyType="done"
+                                    errorMessage={errors.password?.message}
+                                    mainClassName={""}
+                                    onPressLeftIcon={function (): void {
 
-                            {/* Password Field */}
-                            <Animated.View
-                                className="mb-4"
-                                entering={SlideInRight.duration(1400).delay(600)}
-                            >
-                                <Controller
-                                    control={control}
-                                    rules={{
-                                        required: {
-                                            value: true,
-                                            message: JSON.stringify([{ valid: false, title: "Password is Required" }]),
-                                        },
-                                        minLength: {
-                                            value: 6,
-                                            message: JSON.stringify([{ valid: false, title: "Password must be at least 6 characters" }]),
-                                        },
                                     }}
-                                    render={({ field: { onChange, value } }) => (
-                                        <TextField
-                                            label="Password"
-                                            value={value}
-                                            ref={passwordRef}
-                                            rightIcon={showPassword ? "eyeoff" : "eyeOn"}
-                                            onChangeText={onChange}
-                                            secureTextEntry={!showPassword}
-                                            onPressRightIcon={() => setShowPassword(!showPassword)}
-                                            placeholder="Enter your password"
-                                            placeholderTextColor="#9E9E9E"
-                                            mainClassName={""}
-                                            editable={true}
-                                            textInputStyle={undefined}
-                                            mainContainerStyle={""}
-                                            returnKeyType={'done'}
+                                    textInputStyle={undefined} mainContainerStyle={""} />
+                            )}
+                            name="password"
+                        />
+                    </Animated.View>
 
-                                            errorMessage={errors.password?.message}
-                                            onPressLeftIcon={function (): void {
-                                                throw new Error("Function not implemented.");
-                                            }} />
-                                    )}
-                                    name="password"
-                                />
-                            </Animated.View>
-                            <Touchable
-                                entering={SlideInLeft.duration(1600).delay(800)}
-                                onPress={() => {
-                                }}
-                                className="absolute right-5 z-10 bottom-[185px]">
-                                <Text
-                                    className="text-blue-800 text-[14px] font-[Lato-Bold]"
-                                >
-                                    Forgot Password?
-                                </Text>
-                            </Touchable>
-                            <Animated.View
-                                entering={SlideInRight.duration(1800).delay(1000)}>
-                                <CustomButton
-                                    title="Login"
-                                    disabled={!isValid}
-                                    onPress={handleSubmit(handleLogin)}
-                                />
-                            </Animated.View>
-                            <View className="items-center m-5" >
-                                <Text className="text-[18px] font-[Lato-Bold]" >or</Text>
-                            </View>
-                            <Animated.View
-                                entering={SlideInLeft.duration(2000).delay(1200)}>
-                                <CustomButton
-                                    title="Sign in with Google"
-                                    onPress={handleGoogleLogin}
-                                    disabled={false} />
-                            </Animated.View>
-                        </View>
+                    {/* Forgot Password */}
+                    <Touchable
+                        entering={BounceInLeft.duration(2200).delay(1400)}
+                        onPress={() => console.log("Forgot Password Pressed")}
+                        className={'items-end bottom-6'}
+                    >
+                        <Text style={{ color: "blue", fontSize: 14, fontWeight: "bold" }}>
+                            Forgot Password?
+                        </Text>
+                    </Touchable>
 
+                    {/* Login Button */}
+                    <Animated.View entering={BounceInRight.duration(2400).delay(1600)}>
+                        <CustomButton
+                            title="Login"
+                            disabled={!isValid}
+                            onPress={handleSubmit(handleLogin)}
+                            textinPutStyle={""} />
+                    </Animated.View>
+
+                    {/* Separator */}
+                    <View className="items-center my-4">
+                        <Text style={{ fontSize: 18, fontWeight: "bold" }}>or</Text>
                     </View>
+
+                    {/* Google Sign-In Button */}
+                    <Animated.View entering={BounceInRight.duration(2600).delay(1800)}>
+                        <CustomButton
+                            title="Sign in with Google"
+                            onPress={handleGoogleLogin}
+                            disabled={false}
+                            textinPutStyle="text-[#000000]"
+                        />
+                    </Animated.View>
                 </Animated.View>
             </KeyboardAwareScrollView>
-        </LinearGradient>
+        </View >
     );
 };
 
--- app/components/CustomButton/index.tsx
+++ app/components/CustomButton/index.tsx
@@ -3,18 +3,19 @@ import { Text, TouchableOpacity, View } from "react-native";
 
 interface CustomButtonProps {
     title: string,
-    onPress: () => void;
-    disabled: boolean
+    onPress: () => void,
+    disabled: boolean,
+    textinPutStyle?: string,
 }
 
 const CustomButton: React.FC<CustomButtonProps> = (props) => {
 
-    const { title, onPress, disabled } = props
+    const { title, onPress, disabled, textinPutStyle } = props
     return (
         <View>
             <TouchableOpacity onPress={onPress} disabled={disabled}
                 className={`rounded-[10px] ${disabled ? "bg-gray-400" : "bg-[#24BAEC]"}`}>
-                <Text className="text-white p-4 text-[16px] text-center">
+                <Text className={`text-white p-4 text-[16px] text-center ${textinPutStyle} `}>
                     {title}
                 </Text>
             </TouchableOpacity>
